315d4c5c2604303232eb472ff3d7a5deda84d7c2
Guilherme Morgado <56857@novasbe.pt>
2026-03-01 14:31:54 +0000

    Create the function to download the datasets

---
+++ app/data_loader.py
@@ -0,0 +1,30 @@
+import os, requests
+
+def download_datasets(download_dir: str='download') -> None:
+    owid_datasets = {
+        "annual_change_forest_area.csv": "annual-change-forest-area",
+        "annual_deforestation.csv": "annual-deforestation",
+        "share_protected_land.csv": "terrestrial-protected-areas",
+        "share_degraded_land.csv": "share-degraded-land",
+        "share_covered_forest_land.csv": "forest-area-as-share-of-land-area"
+    }
+
+    for filename, slug in owid_datasets.items():
+        url = f"https://ourworldindata.org/grapher/{slug}.csv?v=1"
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+
+        file_path = os.path.join(download_dir, filename)
+        with open(file_path, "wb") as file:
+            file.write(response.content)
+            
+    map_url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries.zip"
+    response = requests.get(map_url, timeout=10)
+    response.raise_for_status()
+    
+    map_path = os.path.join(download_dir, "ne_110m_admin_0_countries.zip")
+    with open(map_path, "wb") as file:
+        file.write(response.content)
+
+if __name__ == "__main__":
+    download_datasets()
